07b1532826bc81e7a648bbe35950fb61c8beebf8
Lilu <KHussell@gmail.com>
2019-06-12 10:47:54 -0400

    server.js imports complete

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require("express")
+var expressHandlebars = require("express-handlebars")
+
+var PORT = process.env.PORT || 8020;
+
+var app = express()
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", expressHandlebars({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.listen(PORT, function(){
+    console.log("Listening")
+})
+
+var routes = require("./controllers/burgers_controller.js")
+app.use(routes)
