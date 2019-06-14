61a290c26ff501e1caa34ee0706c18643f93180e
Lilu <KHussell@gmail.com>
2019-06-14 13:05:41 -0400

    fixed vars

--- config/connection.js
+++ config/connection.js
@@ -2,8 +2,8 @@
 var mysql = require("mysql");
 var connection;
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL)
+if (process.env.JAWSDB_MARIA_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_MARIA_URL)
 }
 else {
   connection = mysql.createConnection({
