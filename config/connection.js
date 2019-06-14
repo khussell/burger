23d874757cbc4a100a5ac637e27cc89b7bb7cf4e
Lilu <KHussell@gmail.com>
2019-06-14 12:27:55 -0400

    working on mysql heroku connection

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,26 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "	c9cujduvu830eexs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-  port: 3306,
-  user: "	tse72b46banq9whs",
-  password: "	drbxme1v88oeuvmr",
-  database: "yxdtrwr60e87syut"
-});
+if (process.env.JAWSDB_URL) {
+  var connection = mysql.createConnection(process.env.JAWSDB_URL)
+}
+else {
+  var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "flamingo3",
+    database: "yxdtrwr60e87syut"
+  });
 
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+  // Make connection.
+  connection.connect(function (err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
+  });
 
-// Export connection for our ORM to use.
-module.exports = connection;
+  // Export connection for our ORM to use.
+  module.exports = connection;
