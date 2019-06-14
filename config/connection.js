b277215b0d4a6deb983d7c4ec32945cc0f8c0b90
Lilu <KHussell@gmail.com>
2019-06-14 12:30:43 -0400

    syntax error

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,19 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
 if (process.env.JAWSDB_URL) {
-  var connection = mysql.createConnection(process.env.JAWSDB_URL)
+   connection = mysql.createConnection(process.env.JAWSDB_URL)
 }
 else {
-  var connection = mysql.createConnection({
+   connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "flamingo3",
     database: "burgers_db"
   });
+}
 
   // Make connection.
   connection.connect(function (err) {
